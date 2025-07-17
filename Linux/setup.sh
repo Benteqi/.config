fb30dbb3698b23fcb4e87ca6b16bdeb19d293c8c
Benteqi on Arch VM <benedykt.cyran@gmail.com>
2025-07-17 23:42:40 +0200

    Configured setup.sh

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+echo "Installing packages..."
+packages = ("fastfetch", "nvim", "hyprland", "wofi", "waybar")
+for package in $packages; do
+	pacman -S $package
+done
+echo "Enabling hyprland autostart..."
 systemctl --user enable ~/config-git/Linux/hyprland.service
+echo "Linking config files..."
+for dir in ~/config-git/Linux/configs/*/; do
+	ln -s ~/config-git/Linux/configs/$dir ~/.config/$dir
+done
+echo "Done"
