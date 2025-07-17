6c9a4f1460d7dcb8032ab8690ab2abd8ee32a7aa
Benteqi on Arch VM <benedykt.cyran@gmail.com>
2025-07-17 20:27:47 +0200

    Added keybinds from the old .vimrc

---
+++ configs/nvim/lua/config/keybinds.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+vim.keymap.set("n", "<Up>", "<Nop>")
+vim.keymap.set("n", "<Down>", "<Nop>")
+vim.keymap.set("n", "<Right>", "<Nop>")
+vim.keymap.set("n", "<Left>", "<Nop>")
