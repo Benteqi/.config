628031a89f8187e91c0a881b86dcbd197ab5c07a
Benteqi on Arch VM <benedykt.cyran@gmail.com>
2025-07-19 22:47:16 +0200

    setup.sh working

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+bash_dir=$(dirname $(readlink -f "$BASH_SOURCE"))
+
 echo "Installing packages..."
 packages=("fastfetch" "nvim" "hyprland" "wofi" "waybar" "kitty" "wikiman" "arch-wiki-docs")
 for package in ${packages[@]}; do
@@ -9,8 +11,10 @@ echo "Enabling hyprland autostart..."
 systemctl --user enable ~/config-git/Linux/hyprland.service
 
 echo "Linking config files..."
-for dir in $("dirname $0")/configs; do
-	ln -s $("dirname $0")/$dir ~/.config/$dir
+for dir in $bash_dir/configs/*/; do
+	dir=${dir%*/}
+	dir="${dir##*/}"
+	ln -fs $bash_dir/configs/$dir ~/.config/$dir
 done
 
 echo "Done"
