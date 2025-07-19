5b5683ad641e73f879e9f458a5adaf6ed61e2f1e
Benteqi on Arch VM <benedykt.cyran@gmail.com>
2025-07-19 17:41:36 +0200

    Added packages to setup.sh

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 echo "Installing packages..."
+<<<<<<< Updated upstream
 packages=("fastfetch" "nvim" "hyprland" "wofi" "waybar")
 for package in $packages; do
 	pacman -S $package
+=======
+packages = ("fastfetch" "nvim" "hyprland" "wofi" "waybar" "kitty" "wikiman" "arch-wiki-docs")
+for package in ${packages[@]}; do
+	sudo pacman -S --noconfirm  $package
+>>>>>>> Stashed changes
 done
+
 echo "Enabling hyprland autostart..."
 systemctl --user enable ~/config-git/Linux/hyprland.service
+
 echo "Linking config files..."
 for dir in ~/config-git/Linux/configs/*/; do
 	ln -s ~/config-git/Linux/configs/$dir ~/.config/$dir
 done
+
 echo "Done"
