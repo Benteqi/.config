dbf56e446b1b2bbc0242ac313c1a1874a08d687a
Benteqi on Arch VM <benedykt.cyran@gmail.com>
2025-08-11 20:28:49 +0200

    Bundled the theme options into a function

--- Linux/configs/nvim/lua/config/theme.lua
+++ Linux/configs/nvim/lua/config/theme.lua
@@ -1,5 +1,8 @@
+function ReloadTheme()
 vim.cmd.colorscheme("tokyonight")
 vim.api.nvim_set_hl(0, "TelescopePromptTitle", {bg = "none", fg = "#ff966c"})
 vim.api.nvim_set_hl(0, "TelescopePromptBorder", {bg = "none", fg = "#ff966c"})
 vim.api.nvim_set_hl(0, "TelescopeBorder", {bg = "none", fg = "#589ed7"})
 vim.api.nvim_set_hl(0, "TelescopeNormal", {bg = "none"})
+end
+ReloadTheme()
