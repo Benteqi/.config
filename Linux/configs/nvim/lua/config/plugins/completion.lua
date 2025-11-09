96ac1903a32e05728c263eb3aaf6073db7560785
Benteqi <benedykt.cyran@gmail.com>
2025-11-09 17:37:42 +0100

    Added lua_ls config

--- Linux/configs/nvim/lua/config/plugins/completion.lua
+++ Linux/configs/nvim/lua/config/plugins/completion.lua
@@ -23,21 +23,54 @@ fuzzy = { implementation = "prefer_rust_with_warning" },
 
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
 		---[[
-		vim.lsp.config("lua_ls", {
-			filetypes={"lua"},
-			capabilities = capabilities,
-			settings = {
-			Lua = {
-			runtime = {
-				version = 'LuaJIT'
-			},
-			diagnostics ={
-				enable = true,
-				globals = { "vim" }
-			}
-			}
-			}
-		})
+		vim.lsp.config('lua_ls', {
+			on_init = function(client)
+				if client.workspace_folders then
+					local path = client.workspace_folders[1].name
+					if
+						path ~= vim.fn.stdpath('config')
+						and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+						then
+							return
+						end
+					end
+
+					client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+						runtime = {
+							-- Tell the language server which version of Lua you're using (most
+							-- likely LuaJIT in the case of Neovim)
+							version = 'LuaJIT',
+							-- Tell the language server how to find Lua modules same way as Neovim
+							-- (see `:h lua-module-load`)
+							path = {
+								'lua/?.lua',
+								'lua/?/init.lua',
+							},
+						},
+						-- Make the server aware of Neovim runtime files
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME
+								-- Depending on the usage, you might want to add additional paths
+								-- here.
+								-- '${3rd}/luv/library'
+								-- '${3rd}/busted/library'
+							}
+							-- Or pull in all of 'runtimepath'.
+							-- NOTE: this is a lot slower and will cause issues when working on
+							-- your own configuration.
+							-- See https://github.com/neovim/nvim-lspconfig/issues/3189
+							-- library = {
+								--   vim.api.nvim_get_runtime_file('', true),
+								-- }
+							}
+						})
+					end,
+					settings = {
+						Lua = {}
+					}
+				})
 		--]]
 		---[[
 		vim.lsp.config("clang", {
@@ -108,7 +141,7 @@ fuzzy = { implementation = "prefer_rust_with_warning" },
 				end
 		})
 
-		vim.lsp.enable({"emmet_language_server", "cssls", "tsserver", "html"})
+		vim.lsp.enable({"emmet_language_server", "cssls", "tsserver", "html", "lua_ls"})
 
 	end,
 	},
