cd4641a92977f74421a0c080d27b5ab3728e126b
Benteqi <benedykt.cyran@gmail.com>
2025-11-11 19:06:40 +0100

    Added visual mode block-wise comment

--- Linux/configs/nvim/lua/config/keybinds.lua
+++ Linux/configs/nvim/lua/config/keybinds.lua
@@ -8,7 +8,8 @@ vim.keymap.set("n", "<Right>", "<Nop>")
 vim.keymap.set("n", "<Left>", "<Nop>")
 
 ---[=[
-function auto_comment()
+local function auto_comment()
+	local keys = ""
 	local syntax_normal = {
 		html = {"<!--", "-->"},
 		css = {"/*", "*/"},
@@ -18,40 +19,60 @@ function auto_comment()
 		lua = {"--"},
 		python = {"#"},
 	}
-	local comment_syntax = syntax_normal
-	--[[
-	if mode == "normal" then 
-		print("")
-	elseif mode == "visual" then
-		comment_syntax = syntax_visual
+	local syntax_visual = {
+		html = {"<!--", "-->"},
+		css = {"/*", "*/"},
+		c = {"/*", "*/"},
+		cpp = {"/*", "*/"},
+		javascript = {"/*", "*/"},
+		lua = {"--[[", "--]]"},
+	}
+	if vim.fn.mode() == "n" then 
+		local line = vim.fn.getline('.')
+		if syntax_normal[vim.bo.filetype] == nil then
+			print("Unsupported filetype!")
+			return ""
+		end
+		local comment = syntax_normal[vim.bo.filetype]
+		if (string.sub(line:gsub("^%s*(.-)%s*$", "%1"), 1, #(comment[1]) + 1)) == comment[1] .. " " and (comment[2] == nil or ((string.sub(line:gsub("^%s*(.-)%s*$", "%1"), #line - #(comment[1]), #line)) == " " .. comment[2])) then
+			keys = "^" .. string.rep("x", #(comment[1])+1)
+			if(comment[2]) then
+				keys = keys .. "$" .. string.rep("x", #(comment[2])+1)
+			end
+		else
+			keys = "^i" .. comment[1] .. " <ESC>"
+			if(comment[2]) then
+				keys = keys .. "$a " .. comment[2] .. "<ESC>"
+			end
+		end
+	elseif vim.fn.mode() == "v" or vim.fn.mode() == "V" then
+		if syntax_visual[vim.bo.filetype] == nil then
+			print("Unsupported filetype!")
+			return ""
+		end
+		local start = vim.fn.getline(vim.fn.line("'<") - 1)
+		local finish = vim.fn.getline(vim.fn.line("'>") + 1)
+		local comment = syntax_visual[vim.bo.filetype]
+		if (start:gsub("%s", "") == comment[1] and (comment[2] == nil or finish:gsub("%s", "") == comment[2])) then
+			keys = "<ESC>\'<kdd" 
+			if(comment[2]) then
+				keys = keys .. "\'>jdd"
+			end
+		else
+			keys = "<ESC>\'<O" .. comment[1] .. " <ESC>"
+			if(comment[2]) then
+				keys = keys .. "\'>o" .. comment[2] .. "<ESC>"
+			end
+		end
 	else 
-		print("Unknown mode")
-		return "<Nop>"
+		print("Unknown mode:", vim.fn.mode())
+		return ""
 	end
-	--]]
-	local keys = ""
-	local line = vim.fn.getline('.')
-	local comment = syntax_normal[vim.bo.filetype]
-	if comment == nil then
-		comment = {"", ""}
-	end
-	if (string.sub(line:gsub("^%s*(.-)%s*$", "%1"), 1, #(comment[1]) + 1)) == comment[1] .. " " and (comment[2] == nil or ((string.sub(line:gsub("^%s*(.-)%s*$", "%1"), #line - #(comment[1]), #line)) == " " .. comment[2])) then
-
-		print("Entered uncomment!")
-		keys = "^" .. string.rep("x", #(comment[1])+1)
-		if(comment[2]) then
-			keys = keys .. "$" .. string.rep("x", #(comment[2])+1)
-		end
-	else
-		print("Entered comment!")
-		keys = "^i" .. comment[1] .. " <ESC>"
-		if(comment[2]) then
-			keys = keys .. "$a " .. comment[2] .. "<ESC>"
-		end
-	end
+	print("Keys returned!")
 	print(keys)
 	return keys
 end
-vim.keymap.set("n", "<C-/>", auto_comment, { expr = true})
+
+vim.keymap.set("x", "<C-/>", auto_comment, { expr = true})
 --]=]
 
